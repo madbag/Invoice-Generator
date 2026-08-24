334b544a85064213c79951be598ae7105de2ea94
Madhushree <madhushree.baliga@gmail.com>
2026-08-24 13:26:21 +0200

    remove unused code

--- frontend/src/pages/Invoices.tsx
+++
@@ -1,9 +0,0 @@
-import InvoiceForm from "../components/Invoices/InvoiceForm";
-
-export default function CreateInvoice() {
-  return (
-    <div className="w-full">
-      <InvoiceForm />
-    </div>
-  );
-}
